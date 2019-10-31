07f8c94c78a8de9c421572519e07baf92d66d94f
Peter Cole <petercole3@gmail.com>
2019-10-31 12:16:52 -0700

    Lab 3 Progress

--- js/app.js
+++ js/app.js
@@ -4,76 +4,97 @@
 
 var player1 = prompt('Hi! What is your name?');
 player1 = player1.toLowerCase();
-alert('name: ' + player1);
-// console.log('name: ' + player1);
+console.log('name: ' + player1);
 
 // Question 1
 
-var ask = prompt('Greetings, ' + player1 + '!\n\nDo you like lions? (yes/no)');
-ask = ask.toLowerCase();
+var ansLions = prompt('Greetings, ' + player1 + '!\n\nDo you like lions? (yes/no)');
+ansLions = ansLions.toLowerCase();
 
-if (ask === ('yes' || 'no')){
-  alert('lions!');
-  // console.log('lions!, answer: ' + ask);
+if (ansLions === 'yes' || ansLions === 'no'){
+  console.log('lions! answer: ' + ansLions);
 } else {
-  alert('knock knock. ya blew it.');
-  // console.log('knock knock. ya blew it.');
+  console.log(ansLions + '. Ya blew it.');
 }
 
 // Question 2
 
-ask = prompt('Do you like tigers? (yes/no)');
-ask = ask.toLowerCase();
+var ansTigers = prompt('Do you like tigers? (yes/no)');
+ansTigers = ansTigers.toLowerCase();
 
-if (ask === 'yes' || ask === 'no'){
-  alert('tigers!');
-  // console.log('tigers! answer: ' + ask);
+if (ansTigers === 'yes' || ansTigers === 'no'){
+  console.log('tigers! answer: ' + ansTigers);
 } else {
-  alert('knock knock. ya blew it.');
-  // console.log('knock knock. ya blew it.');
+  console.log(ansTigers + '. Ya blew it.');
 }
 
 // Question 3
 
-ask = prompt('Do you like bears? (yes/no)');
-ask = ask.toLowerCase();
+var ansBears = prompt('Do you like bears? (yes/no)');
+ansBears = ansBears.toLowerCase();
 
-if (ask === 'yes' || ask === 'no'){
-  alert('bears!');
-  // console.log('bears! answer: ' + ask);
+if (ansBears === 'yes' || ansBears === 'no'){
+  console.log('bears! answer: ' + ansBears);
 } else {
-  alert('knock knock. ya blew it.');
-  // console.log('knock knock. ya blew it.');
+  console.log(ansBears + '. Ya blew it.');
 }
 
 // Question 4
 
-ask = prompt('Have you ever said "OH MY!" (yes/no)');
-ask = ask.toLowerCase();
+var ansOhMy = prompt('Have you ever said "OH MY!" (yes/no)');
+ansOhMy = ansOhMy.toLowerCase();
 
-if (ask === 'yes' || ask === 'no'){
-  alert('previous expression of alarm');
-  // console.log('previous expression of alarm, answer: ' + ask);
+if (ansOhMy === 'yes' || ansOhMy === 'no'){
+  console.log('previous expression of alarm, answer: ' + ansOhMy);
 } else {
-  alert('knock knock. ya blew it.');
-  // console.log('knock knock. ya blew it.');
+  console.log(ansOhMy + '. Ya blew it.');
 }
 
 // Question 5
 
-ask = prompt('Has a house ever fallen on a close relative? (yes/no)');
-ask = ask.toLowerCase();
+var ansHouse = prompt('Has a house ever fallen on a close relative? (yes/no)');
+ansHouse = ansHouse.toLowerCase();
 
-if (ask === 'yes' || ask === 'no'){
-  alert('prior OSHA claim');
-  // console.log('prior OSHA claim, answer: ' + ask);
+if (ansHouse === 'yes' || ansHouse === 'no'){
+  console.log('prior OSHA claim, answer: ' + ansHouse);
 } else {
-  alert('knock knock. ya blew it.');
-  // console.log('knock knock. ya blew it.');
+  console.log(ansHouse + '. Ya blew it.');
 
 }
 
-alert('Thank you for letting me run you through some questions, ' + player1 + '!');
+// Question 6 Number Game
+
+var myNumber = Math.floor(Math.random() * 10) +1; // creates random # 1 - 10
+console.log(myNumber);
+
+var numberOfGuessesLeft = 4;
+var correctFlag = 1;
+
+while ((numberOfGuessesLeft !== 0) && (correctFlag === 1)) {
+  var ansNumber = prompt('Guess a number between 1 and 10');
+  console.log(ansNumber);
+  console.log(correctFlag);
+  numberOfGuessesLeft--;
+  console.log(numberOfGuessesLeft);
+  if (ansNumber == myNumber) {
+    correctFlag--;
+  }
+  if (ansNumber > myNumber) {
+    alert('Too high!');
+  }
+  if (ansNumber < myNumber) {
+    alert('Too low!');
+  }
+}
+
+if (correctFlag !== 1){
+  alert('Great job on the correct answer, ' + player1 + '!\n\nThank you for playing!')
+} else {
+  alert('The correct answer was ' + myNumber);
+  alert('Thank you for letting me run you through some questions, ' + player1 + '!');
+  console.log('name: ' + ansLions, '\nTigers: ' + ansTigers,
+    '\nBears: ' + ansBears, '\nOh My: ' + ansOhMy, '\nHome-icide: ' + ansHouse);
+}
 
 // Peter - when we get arrays, try using one variable
 // and cycling the prompts and logs as strings? 10/29/19
